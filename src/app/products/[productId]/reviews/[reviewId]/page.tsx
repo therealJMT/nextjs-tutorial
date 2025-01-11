a6e5004b263db827b1518a8bf6a27c9cf3c00dca
Matthias Metzger <mmetzger1987@googlemail.com>
2025-01-11 11:26:53 +0100

    26 error handling complete

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,16 +1,24 @@
 import { notFound, redirect } from "next/navigation";
 
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default async function ReviewDetails({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
+  const random = getRandomInt(2);
+  if(random ===1) {
+    throw new Error("Error loading review")
+  }
   const productId = await params.productId;
   const reviewId = await params.reviewId;
 
   if (parseInt(reviewId) > 1000) {
     // notFound();
-    redirect("/products");
+    notFound();
   }
   return (
     <>
